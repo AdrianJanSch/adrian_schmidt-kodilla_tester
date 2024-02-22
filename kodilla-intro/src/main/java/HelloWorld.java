e0e0698de23ac9579b9d5442e05e4aa530c66925
Adrian Schmidt <adrjansch96@gmail.com>
2024-02-22 21:13:39 +0100

    git push

--- kodilla-intro/src/main/java/HelloWorld.java
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
-    public static   void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String [] args) {
+        System.out.println("HelloWorld");
         System.out.println("Adrian");
         System.out.println("27");
         System.out.println("S");
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,19 @@
+public class MainExtended {
+    public static void main(String [] args){
+    String miasto = "-Chorzów";
+    String pochodzenie = "-Skąd pochodzisz?";
+    int age = -27;
+    String wiek = "-Ile masz lat?";
+    double heigh = -1.85;
+    String wzrost = "-Jaki wzrost Twój?";
+    boolean tester = true;
+        System.out.println(pochodzenie);
+        System.out.println(miasto);
+        System.out.println(wiek);
+        System.out.println(age);
+        System.out.println(wzrost);
+        System.out.println(heigh);
+        System.out.println("Czy będziesz testerem?");
+        System.out.println(tester);
+}
+}
