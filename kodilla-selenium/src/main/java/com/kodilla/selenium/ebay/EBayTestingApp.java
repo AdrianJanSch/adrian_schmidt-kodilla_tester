351c82bba3c1d6acba65fc55b6747d99ba6d6a1b
Adrian Schmidt <adrjansch96@gmail.com>
2024-06-16 11:08:33 +0200

    git push git push

--- kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/ebay/EBayTestingApp.java
@@ -4,15 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 public class EBayTestingApp {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver5.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.ebay.com");
-        WebElement inputField = driver.findElement(By.name("_nkw")); // [4]
+        WebElement inputField = driver.findElement(By.name("_nkw"));
         inputField.sendKeys("laptop");
         inputField.submit();
 
